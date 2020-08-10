fd538b621b7968fd6152ed624bd9c327c5d1e0b7
Marcos Vidal <marcos.vidal.007@gmail.com>
2020-08-10 13:40:29 -0300

    docs agregado

---
+++ docs/precache-manifest.ccb179eb707ae7236d0b7c2a810fe3ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e032d839bbf33ca3b48b3e2ce9faad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a75312cc439f2137c1",
+    "url": "/static/css/main.347d5aec.chunk.css"
+  },
+  {
+    "revision": "3128dbdfc38ae3cbab54",
+    "url": "/static/js/2.e0589b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0589b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a75312cc439f2137c1",
+    "url": "/static/js/main.bfb15653.chunk.js"
+  },
+  {
+    "revision": "639219b2ad910fd9acf9",
+    "url": "/static/js/runtime-main.b7a9bcd2.js"
+  },
+  {
+    "revision": "111c034841b456156aee2f7db475e76d",
+    "url": "/static/media/bg.111c0348.jpg"
+  },
+  {
+    "revision": "57aef36ffc811f417d6958ab13cfc872",
+    "url": "/static/media/noimage.57aef36f.png"
+  }
+]);
