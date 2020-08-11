a5104b037ba5a4450ef9f43ff726be4c30b247aa
Marcos Vidal <marcos.vidal.007@gmail.com>
2020-08-10 21:31:12 -0300

    rutas de imagenes actualizadas

--- src/components/MoviesGridItem.js
+++ src/components/MoviesGridItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ContentCard } from './ContentCard';
-import noimage from '../../docs/static/media/noimage.57aef36f.png'
+import noimage from '../docs/static/media/noimage.57aef36f.png'
 
 export const MoviesGridItem = ( { title, url, poster, urlFull, popularity, descr, vote} ) => {
 
@@ -10,7 +10,7 @@ export const MoviesGridItem = ( { title, url, poster, urlFull, popularity, descr
 
     if (poster === null) {
         urlFull= noimage;
-    } else{
+    } else {
         urlFull = url+poster;
     }
 
